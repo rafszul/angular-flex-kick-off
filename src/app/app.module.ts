03fe058ca0c9b6460b28232bad9dbba5be04b9c0
@rafszul <rafszul@gmail.com>
2017-01-07 00:33:05 +0000

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { RoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SecondComponent } from './second/second.component';
+import { FirstchildComponent } from './firstchild/firstchild.component';
+import { SecondchildComponent } from './secondchild/secondchild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SecondComponent,
+    FirstchildComponent,
+    SecondchildComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    FlexLayoutModule.forRoot()
+    FlexLayoutModule.forRoot(),
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/firstchild/firstchild.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-firstchild',
+  templateUrl: './firstchild.component.html',
+  styleUrls: ['./firstchild.component.scss']
+})
+
+export class FirstchildComponent implements OnInit {
+  value;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  callfirst() {
+    this.value = 1;
+    this.router.navigate(['/second', { outlets: { 'secondchild': [this.value] } }]);
+  }
+
+  callsecond() {
+    this.value = 2;
+    this.router.navigate(['/second', { outlets: { 'secondchild': [this.value] } }]);
+  }
+
+}
